a688ebb62705d10ad5ba1ace166100ca48a8049b
Eliza Sinkevich <sinkevicheliza@gmail.com>
2020-05-14 00:33:20 +0200

    Add files via upload
    initial commit

---
+++ constructors/src/PrzykladPrzeciazanie.java
@@ -0,0 +1,15 @@
+public class PrzykladPrzeciazanie {
+
+
+    public void wypisz(int i){
+        System.out.println("int = " + i);
+    }
+
+    public void wypisz(double d){
+        System.out.println("double = " + d);
+    }
+
+    public void wypisz(String str){
+        System.out.println("String = " + str);
+    }
+}
---
+++ constructors/src/Zad4.java
@@ -0,0 +1,8 @@
+public class Zad4 {
+    public static void main(String[] args) {
+        PrzykladPrzeciazanie przykladPrzeciazanie = new PrzykladPrzeciazanie();
+        przykladPrzeciazanie.wypisz(10);
+        przykladPrzeciazanie.wypisz(19.2);
+        przykladPrzeciazanie.wypisz("Przeciazanie argument tekst");
+    }
+}
